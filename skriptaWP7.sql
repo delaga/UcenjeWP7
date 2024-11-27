7d7c55daa07c7af3f741e5b06f0bd5b17b7db9cd
MirzaTheCar <mirza.delagic@gmail.com>
2024-11-27 19:20:06 +0100

    Update skripta

--- skriptaWP7.sql
+++ skriptaWP7.sql
@@ -1,7 +1,14 @@
-﻿--neki komentar
---create database edunovawp7;
---use edunovawp7;
-create table smijer(
-	naziv varchar (50)
-);
+﻿-- Ovako se pišu komentari
 
+use master;
+go
+drop database if exists edunovawp7;
+go
+create database edunovawp7;
+go
+use edunovawp7;
+go
+
+create table smjer(
+naziv varchar(50)
+);
