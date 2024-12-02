772e9680c67b71b62913d3b9122961a40b40fb95
MirzaTheCar <mirza.delagic@gmail.com>
2024-12-02 20:11:11 +0100

    Z2 gotov

--- nogomet.sql
+++ nogomet.sql
@@ -18,5 +18,30 @@ create table klubovi(
 );
 
 create table utakmice(
-	
+	sifra int not null primary key identity(1,1),
+	datum date not null,
+	vrijeme time,
+	lokacija varchar(100),
+	stadion varchar(100),
+	domaciKlub int not null references klubovi(sifra),
+	gostujuciKlub int not null references klubovi(sifra) 
+);
+
+create table igraci (
+	sifra int not null primary key identity(1,1),
+	ime varchar(50),
+	prezime varchar(50),
+	datumRodjenja date,
+	pozicija varchar(50),
+	brojDresa int,
+	klub int not null references klubovi(sifra)
+);
+
+create table treneri (
+	sifra int not null primary key identity(1,1),
+	ime varchar(50),
+	prezime varchar(50),
+	nacionalnost varchar(50),
+	klub int not null references klubovi(sifra),
+	iskustvo varchar(50)
 );
