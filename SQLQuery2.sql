fd5b628f74cf8120a00d9c5a5efc4d97238f14d1
Mirza Delagic <mirza.delagic@gmail.com>
2024-12-11 20:58:43 +0100

    ajaj

---
+++ SQLQuery2.sql
@@ -0,0 +1,31 @@
+﻿select * from autor order by prezime asc,ime desc;
+
+select prezime,ime from autor order by 1 asc,2 desc;
+
+select top 10 percent * from autor;
+
+select top 10 * from mjesto;
+
+select top 10 * into nova from mjesto;
+
+select * from nova;
+
+drop table nova;
+
+select * from autor;
+
+insert into autor(sifra,ime,prezime,datumrodenja) values (4,'Mirza','Delagić','1975-09-18 16:50:00');
+
+
+select * from katalog;
+select * from katalog where naslov like '%ljubav%'and sifra in (2541,2660,2664,2938);
+
+select count(*) from izdavac where naziv like '%d.o.o.';
+
+select count(*) from izdavac where naziv like '%d.o.o.' or naziv like '%doo';
+select top 20 * from izdavac;
+
+select * from izdavac where naziv like '%d.%o%o%' or naziv like '%d%o%o.%';
+
+
+
---
+++ select.sql
@@ -0,0 +1,45 @@
+﻿select * from smjerovi;
+
+select 1;
+
+select *, naziv, 1, getdate() from smjerovi;
+
+select *, naziv, 1 as iznos, getdate() as datum from smjerovi;
+
+select naziv as predmet, getdate() as datumpocetka from smjerovi;
+
+select sifra, naziv from smjerovi;
+
+select ime, prezime from polaznici;
+
+select * from smjerovi where sifra=1;
+
+select * from smjerovi where sifra!=2;
+
+select * from smjerovi where sifra>2 and sifra<4; --svi uvijeti AND
+
+select * from smjerovi where sifra=1 or sifra=4; -- barem jedan OR
+
+select * from smjerovi where not (sifra=1 or sifra=4);
+
+select * from polaznici where prezime='Nađ';
+
+select * from polaznici where prezime like '%an%';
+
+select * from polaznici where ime like '%a';
+
+select * from polaznici where sifra between 10 and 15;
+
+select * from polaznici where sifra in (10,15,1);
+
+
+select * from smjerovi;
+
+update smjerovi set izvodiseod='2024-01-02 18:30'where sifra=2;
+update smjerovi set izvodiseod='2024-04-02 18:30'where sifra=3;
+update smjerovi set izvodiseod='2024-10-02 18:30'where sifra=4;
+
+select * from smjerovi;
+
+
+select * from smjerovi where izvodiseod between '2024-01-01' and '2024-06-30';
