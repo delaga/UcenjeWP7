839395fe8d414efcb00985f8505e35f8238eb139
Mirza Delagic <mirza.delagic@gmail.com>
2024-12-20 19:05:46 +0100

    Plus Tomislav

---
+++ Tomislav/CSHARP/Ucenje/E02Varijable.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E02Varijable
+    {
+
+        public static void Izvedi()
+        {
+
+            //Console.WriteLine("E02");
+
+            // Tipovi podataka
+
+            int cijeliBroj = 1; // ovo je skraće kao da smo učitali od korisnika
+
+            bool logickaVrijednost = true; // zadana vrijednost je false
+
+            float decimalniBroj = 4.5F;
+
+            double velikiDecimalniBroj = 3.14;
+
+            decimal decimalniBroj2 = 3.4M;
+
+            char znak = '@';
+
+            string nizZnakova = "abcdefg";
+
+            Console.WriteLine("Znak je broj {0}", (int)znak); // (int) je cast
+
+            cijeliBroj = int.MaxValue;
+
+            Console.WriteLine(cijeliBroj);
+            Console.WriteLine(cijeliBroj+1);
+
+
+
+        }
+
+
+
+
+    }
+}
---
+++ Tomislav/CSHARP/Ucenje/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using Ucenje;
+
+E03Operatori.Izvedi();
+
+//E02Varijable.Izvedi();
+
+//E01Z1.Izvedi();
+
+
+//E01UlazIzlaz.Izvedi();
+
+
+//Console.WriteLine("Hello, World!");
