c04e40374d41384c5d494f0e1355447cbfd8c97d
Mirza Delagic <mirza.delagic@gmail.com>
2024-12-30 14:58:53 +0100

    ZZ05

--- CSHARP/Ucenje/Program.cs
+++ CSHARP/Ucenje/Program.cs
@@ -32,5 +32,5 @@ using Ucenje;
 
 //Console.WriteLine("Hello, World!");
 //E08forPetlja.Run();
-ZimskiZadaci04.Run();
+ZimskiZadaci05.Run();
 
---
+++ CSHARP/Ucenje/ZimskiZadaci05.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class ZimskiZadaci05
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Unesi broj elemenata fibe: ");
+            int n=int.Parse(Console.ReadLine());
+
+            if (n<=0)
+            {
+                Console.WriteLine("Ne seri");
+                return;
+            }
+
+            Console.WriteLine("Fibijev niz do tvog elementa:");
+            int prethodni = 0, trenutni = 1;
+            for (int i = 0; i < n; i++) 
+            {
+                
+                
+                Console.WriteLine(prethodni);
+                int sljedeci = prethodni + trenutni;
+                prethodni = trenutni;
+                trenutni = sljedeci;
+            }
+        }
+    }
+}
