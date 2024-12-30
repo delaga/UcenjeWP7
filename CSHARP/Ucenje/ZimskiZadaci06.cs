b30af1cf285615e81a23c5598a0d8fe9defb27e4
Mirza Delagic <mirza.delagic@gmail.com>
2024-12-30 15:47:18 +0100

    ZZ06

---
+++ CSHARP/Ucenje/ZimskiZadaci06.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class ZimskiZadaci06
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Unesi neku riječ pa da ju obrnemo, hvala");
+            string rijec = Console.ReadLine();
+
+            string cejir = "";
+
+            for (int i = rijec.Length - 1; i >= 0; i--)
+            {
+                cejir += rijec[i];
+            }
+            Console.WriteLine("Rijec {0} je {1}", rijec, cejir);
+        }
+    }
+}
